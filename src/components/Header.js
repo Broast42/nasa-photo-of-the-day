9d207b73369112d4f7bea6aa11ab1f3575a7a4dc
rdowd <rjarrettdowd@yahoo.com>
2020-01-09 19:21:32 -0500

    added styled components in header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+   background: #002db3;
+   color: white;
+   height: 150px;
+   border-bottom: 10px solid #cc0000;
+   text-align: center;
+   padding: 20px; 
+`;
+
+const StyledH1 = styled.h1`
+    font-size: 3rem;
+`;
+
+const StyledP = styled.p`
+    font-size: 2rem;
+`;
 
 export default function Header(props){
 
     return(
-        <header>
-            <h1>Astronomy Picture of the Day</h1>
-            <p>{props.date}</p>
-        </header>
+        <StyledHeader>
+            <StyledH1>Astronomy Picture of the Day</StyledH1>
+            <StyledP>{props.date}</StyledP>
+        </StyledHeader>
     );
 }
